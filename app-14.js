6382221564abfb47dbb946bb65f251113df279f6
EunJin <91128036+MeungGenie@users.noreply.github.com>
2024-11-14 11:09:58 +0900

    Add files via upload

--- app-14.js
+++ app-14.js
@@ -15,7 +15,7 @@ dbConnect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/contacts", require("./routes/contactRoutes-5"));
+app.use("/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
   console.log(`${port}번 포트에서 서버 실행 중`);
